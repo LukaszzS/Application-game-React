6b99b21a46d8ce4cb8e33a41bb9a0fd0275fa21a
Łukasz Szlaski <lucashlszl@gmail.com>
2019-09-18 19:12:22 +0200

    settings - improved

--- src/Settings/Settings.js
+++ src/Settings/Settings.js
@@ -12,33 +12,36 @@ class Settings extends React.Component {
 
   render() {
     return (
-      <div className="container__settings">
-          <div className="container__settings__text">
-              <div className="settings__general">
-                  <h1>{lang[localStorage.getItem("lang")].gobalSettings}</h1>
-                  <div>
-                    <h3>{lang[localStorage.getItem("lang")].pleaseSelect}</h3>
-
-                    <button className="btn btn-danger" onClick={this.setLang.bind(this, 'pl')}>PL</button>
-                    <button className="btn btn-primary" onClick={this.setLang.bind(this, 'en')}>EN</button>
-                  </div>
-              </div>
-              <div className="settings__tictactoe">
-                  <h2>{lang[localStorage.getItem("lang")].settingsTicTacToe}</h2>
-                  <div>
-                    <h3>{lang[localStorage.getItem("lang")].pleaseNumberOfPlayers}</h3>
-                    <button className="btn btn-outline-danger">1</button>
-                    <button className="btn btn-outline-primary">2</button>
-                  </div>
-              </div>
-              <div className="settings__paddle">
-                  <h2>{lang[localStorage.getItem("lang")].settingsPaddlegame}</h2>
-                  <div>
-                    <h3>{lang[localStorage.getItem("lang")].pleaseSetTheSpeedTheBall}</h3>
-                    <input type="range" name="points" min="1" max="50" className="input_range"></input>
-                  </div>
-              </div>
-          </div>
+      <div className="settings_img">
+        <div className="settings">
+        <h1>{lang[localStorage.getItem("lang")].gobalSettings}</h1>
+            <div className="settings settings__box">
+                <div className="settings__title">
+                      <h2>{lang[localStorage.getItem("lang")].pleaseSelect}</h2>
+                      <button className="btn btn-danger" onClick={this.setLang.bind(this, 'pl')}>PL</button>
+                      <button className="btn btn-primary" onClick={this.setLang.bind(this, 'en')}>EN</button>
+                </div>
+            </div>
+            <div className="settings settings__box">
+                <div className="settings__title">
+                    <h2>{lang[localStorage.getItem("lang")].settingsTicTacToe}</h2>
+                    <div>
+                      <h3>{lang[localStorage.getItem("lang")].pleaseNumberOfPlayers}</h3>
+                      <button className="btn btn-outline-danger">1</button>
+                      <button className="btn btn-outline-primary">2</button>
+                    </div>
+                </div>
+            </div>
+            <div className="settings settings__box">
+                <div className="settings__title">
+                    <h2>{lang[localStorage.getItem("lang")].settingsPaddlegame}</h2>
+                    <div>
+                      <h3>{lang[localStorage.getItem("lang")].pleaseSetTheSpeedTheBall}</h3>
+                      <input type="range" name="points" min="1" max="50" className="input_range"></input>
+                    </div>
+                </div>
+            </div>
+        </div>
       </div>
     );
   }
